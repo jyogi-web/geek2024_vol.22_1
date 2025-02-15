c6cfcb4633cf6dfad311918ffc16fff6bb033b22
NazonoKansatugata <0gy27b297263j6s@au.com>
2025-02-15 15:40:35 +0900

    お気に入り登録をユーザーごとに実装
    ホームページとお気に入り画面をいじりました

--- lib/ui/favorite/favorite_page.dart
+++ lib/ui/favorite/favorite_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../home/ProfileCard.dart'; // プロフィールカードのインポート
 
 class FavoriteScreen extends StatelessWidget {
@@ -12,22 +13,40 @@ class FavoriteScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('profiles').where('isFavorite', isEqualTo: true).snapshots(),
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser?.uid)
+            .collection('likedProfiles')
+            .snapshots(),  // likedProfilesを参照
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
           if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Center(child: Text("お気に入りがありません"));
+            return Center(child: Text("お気に入りのプロフィールはありません"));
           }
 
-          var favoriteProfiles = snapshot.data!.docs;
+          var likedProfiles = snapshot.data!.docs;
 
           return ListView.builder(
-            itemCount: favoriteProfiles.length,
+            itemCount: likedProfiles.length,
             itemBuilder: (context, index) {
-              var profile = favoriteProfiles[index].data() as Map<String, dynamic>;
-              return ProfileCard(profile: profile, documentId: favoriteProfiles[index].id);
+              var profileId = likedProfiles[index].id;
+
+              return FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance.collection('profiles').doc(profileId).get(),
+                builder: (context, profileSnapshot) {
+                  if (profileSnapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (!profileSnapshot.hasData || !profileSnapshot.data!.exists) {
+                    return SizedBox.shrink(); // プロフィールが存在しない場合、何も表示しない
+                  }
+
+                  var profile = profileSnapshot.data!.data() as Map<String, dynamic>;
+                  return ProfileCard(profile: profile, documentId: profileId);
+                },
+              );
             },
           );
         },
